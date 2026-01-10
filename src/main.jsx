343e11ab75c11ffc44ff3fc1be8479d3a70edfd4
leekyuhyun <leekh010502@gmail.com>
2026-01-10 13:03:23 +0900

    fix : components -> HomeView -> main.jsx의 순서 변경으로 인한 App.jsx 삭제 및 css 수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
+import './main.css';
+import HomeView from './views/HomeView';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HomeView />
   </StrictMode>,
 );
