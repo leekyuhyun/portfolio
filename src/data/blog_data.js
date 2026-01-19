b16e072de833aef7d8e0b1983f254027c4e2fb13
leekyuhyun <leekh010502@gmail.com>
2026-01-19 04:06:33 +0000

    chore : update latest velog posts

--- src/data/blog_data.js
+++ src/data/blog_data.js
@@ -1,5 +1,11 @@
 export const blogData = [
   {
+    "title": "프로그래머스 웹 풀스택 3주 (01월 19일)",
+    "date": "2026. 1. 19.",
+    "url": "https://velog.io/@leekh010502/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%9B%B9-%ED%92%80%EC%8A%A4%ED%83%9D-3%EC%A3%BC-01%EC%9B%94-19%EC%9D%BC",
+    "summary": "Express, REST API 실습, Json\nHTTP vs Express\nHTTP\nNode.js 설치 시 기본적으로 제공되는 가장 기초적인 모듈\nExpress\nHTTP 모듈을 ..."
+  },
+  {
     "title": "프로그래머스 웹 풀스택 2주 (01월 16일)",
     "date": "2026. 1. 16.",
     "url": "https://velog.io/@leekh010502/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%9B%B9-%ED%92%80%EC%8A%A4%ED%83%9D-2%EC%A3%BC-01%EC%9B%94-16%EC%9D%BC",
@@ -16,11 +22,5 @@ export const blogData = [
     "date": "2026. 1. 14.",
     "url": "https://velog.io/@leekh010502/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%9B%B9-%ED%92%80%EC%8A%A4%ED%83%9D-2%EC%A3%BC-01%EC%9B%94-14%EC%9D%BC",
     "summary": "웹 포트폴리오 제작 및 데이터베이스 모델링\n웹 포트폴리오 제작\nHTML + CSS로 간단한 웹 포트폴리오 제작을 했습니다\n결과 화면\n  \n링크\nkyulee's portfolio\n데..."
-  },
-  {
-    "title": "프로그래머스 웹 풀스택 2주 (01월 13일)",
-    "date": "2026. 1. 13.",
-    "url": "https://velog.io/@leekh010502/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%9B%B9-%ED%92%80%EC%8A%A4%ED%83%9D-2%EC%A3%BC-01%EC%9B%94-13%EC%9D%BC",
-    "summary": "데이터베이스 / 실전 프로젝트 (2)\n데이터베이스\n데이터베이스란\n데이터를 통합하여 효율적으로 관리하기 위한 데이터 집합체를 데이터베이스(Database,줄여서 DB)\n데이터를 구조..."
   }
 ];
