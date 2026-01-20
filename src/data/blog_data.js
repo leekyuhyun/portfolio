eb917f41185eff0f2b864a715a758fbd7acc905b
leekyuhyun <leekh010502@gmail.com>
2026-01-20 04:01:04 +0000

    chore : update latest velog posts

--- src/data/blog_data.js
+++ src/data/blog_data.js
@@ -1,5 +1,11 @@
 export const blogData = [
   {
+    "title": "Node.js 기본 생태계",
+    "date": "2026. 1. 19.",
+    "url": "https://velog.io/@leekh010502/Node.js-%EA%B8%B0%EB%B3%B8-%EC%83%9D%ED%83%9C%EA%B3%84",
+    "summary": "Node.js 패키지 매니저\nNode.js 생태계에서 프로젝트 의존성을 관리하는 대표적인 도구 3가지를 정리\n\n1. NPM (Node Package Manager)\nNode.js ..."
+  },
+  {
     "title": "프로그래머스 웹 풀스택 3주 (01월 19일)",
     "date": "2026. 1. 19.",
     "url": "https://velog.io/@leekh010502/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%9B%B9-%ED%92%80%EC%8A%A4%ED%83%9D-3%EC%A3%BC-01%EC%9B%94-19%EC%9D%BC",
@@ -16,11 +22,5 @@ export const blogData = [
     "date": "2026. 1. 15.",
     "url": "https://velog.io/@leekh010502/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%9B%B9-%ED%92%80%EC%8A%A4%ED%83%9D-2%EC%A3%BC-01%EC%9B%94-15%EC%9D%BC",
     "summary": "API, REST API, URL\nAPI란?\nApplication Programming Interface\n서로 다른 소프트웨어가 데이터를 주고받거나 기능을 호출할 수 있도록 규격화..."
-  },
-  {
-    "title": "프로그래머스 웹 풀스택 2주 (01월 14일)",
-    "date": "2026. 1. 14.",
-    "url": "https://velog.io/@leekh010502/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%9B%B9-%ED%92%80%EC%8A%A4%ED%83%9D-2%EC%A3%BC-01%EC%9B%94-14%EC%9D%BC",
-    "summary": "웹 포트폴리오 제작 및 데이터베이스 모델링\n웹 포트폴리오 제작\nHTML + CSS로 간단한 웹 포트폴리오 제작을 했습니다\n결과 화면\n  \n링크\nkyulee's portfolio\n데..."
   }
 ];
