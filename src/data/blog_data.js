b4581758af495f0523db91b328fead4a0288fcea
leekyuhyun <leekh010502@gmail.com>
2026-01-21 04:00:51 +0000

    chore : update latest velog posts

--- src/data/blog_data.js
+++ src/data/blog_data.js
@@ -1,5 +1,11 @@
 export const blogData = [
   {
+    "title": "[Baseball Starter] 야구 입문자를 위한 커뮤니티 플랫폼 기획 ⚾️",
+    "date": "2026. 1. 20.",
+    "url": "https://velog.io/@leekh010502/%EC%95%BC%EA%B5%AC-%EC%9E%85%EB%AC%B8%EC%9E%90%EB%A5%BC-%EC%9C%84%ED%95%9C-%EC%BB%A4%EB%AE%A4%EB%8B%88%ED%8B%B0-%ED%94%8C%EB%9E%AB%ED%8F%BC-Baseball-Starter-%EA%B8%B0%ED%9A%8D%EA%B8%B0",
+    "summary": "1. 프로젝트 개요\n최근 KBO 리그의 인기가 급증하면서 야구장을 찾는 '야린이(야구 입문자)'들이 많아졌습니다. 하지만 야구는 규칙이 복잡하고, 팀별 응원 문화나 정보가 파편화되..."
+  },
+  {
     "title": "Node.js 기본 생태계",
     "date": "2026. 1. 19.",
     "url": "https://velog.io/@leekh010502/Node.js-%EA%B8%B0%EB%B3%B8-%EC%83%9D%ED%83%9C%EA%B3%84",
@@ -16,11 +22,5 @@ export const blogData = [
     "date": "2026. 1. 16.",
     "url": "https://velog.io/@leekh010502/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%9B%B9-%ED%92%80%EC%8A%A4%ED%83%9D-2%EC%A3%BC-01%EC%9B%94-16%EC%9D%BC",
     "summary": "Node.js, 모듈, 라이브러리, 프레임워크\nNode.js\n특징\n싱글 스레드\n한 번에 한 가지 일만 처리하는 일꾼이 한 명뿐이지만, 일을 매우 빠르게 번갈아 처리하여 효율적으로 ..."
-  },
-  {
-    "title": "프로그래머스 웹 풀스택 2주 (01월 15일)",
-    "date": "2026. 1. 15.",
-    "url": "https://velog.io/@leekh010502/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%9B%B9-%ED%92%80%EC%8A%A4%ED%83%9D-2%EC%A3%BC-01%EC%9B%94-15%EC%9D%BC",
-    "summary": "API, REST API, URL\nAPI란?\nApplication Programming Interface\n서로 다른 소프트웨어가 데이터를 주고받거나 기능을 호출할 수 있도록 규격화..."
   }
 ];
