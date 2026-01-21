3a5bcf6abac454244720b7ad8585b8e3e51b76e0
leekyuhyun <leekh010502@gmail.com>
2026-01-21 13:46:08 +0000

    chore : update latest velog posts

--- src/data/blog_data.js
+++ src/data/blog_data.js
@@ -1,5 +1,17 @@
 export const blogData = [
   {
+    "title": "[Baseball Starter] 와이어프레임 제작 (1) ✏️ ",
+    "date": "2026. 1. 21.",
+    "url": "https://velog.io/@leekh010502/Baseball-Starter-%EC%99%80%EC%9D%B4%EC%96%B4%ED%94%84%EB%A0%88%EC%9E%84-%EC%A0%9C%EC%9E%91-1",
+    "summary": "이번 시간에는 와이어프레임을 Figma로 그려서 가져왔습니다. \n아직 전부 다 그리진 못했지만 완성한 곳 까지만 가져와봤습니다. 😅\n로그인\n제일 제작하기 쉬운 로그인 화면을 가져..."
+  },
+  {
+    "title": "프로그래머스 웹 풀스택 3주 (01월 21일)",
+    "date": "2026. 1. 21.",
+    "url": "https://velog.io/@leekh010502/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%9B%B9-%ED%92%80%EC%8A%A4%ED%83%9D-3%EC%A3%BC-01%EC%9B%94-21%EC%9D%BC",
+    "summary": "자바스크립트 네이밍룰 / req.params 연습 / 객체, 배열 비구조화 /  map,형변환\n자바스크립트 네이밍 룰\ncamelCase\n두 번째 이상의 단어는 첫 글자를 대문자로 ..."
+  },
+  {
     "title": "[Baseball Starter] 야구 입문자를 위한 커뮤니티 플랫폼 기획 ⚾️",
     "date": "2026. 1. 20.",
     "url": "https://velog.io/@leekh010502/%EC%95%BC%EA%B5%AC-%EC%9E%85%EB%AC%B8%EC%9E%90%EB%A5%BC-%EC%9C%84%ED%95%9C-%EC%BB%A4%EB%AE%A4%EB%8B%88%ED%8B%B0-%ED%94%8C%EB%9E%AB%ED%8F%BC-Baseball-Starter-%EA%B8%B0%ED%9A%8D%EA%B8%B0",
@@ -10,17 +22,5 @@ export const blogData = [
     "date": "2026. 1. 19.",
     "url": "https://velog.io/@leekh010502/Node.js-%EA%B8%B0%EB%B3%B8-%EC%83%9D%ED%83%9C%EA%B3%84",
     "summary": "Node.js 패키지 매니저\nNode.js 생태계에서 프로젝트 의존성을 관리하는 대표적인 도구 3가지를 정리\n\n1. NPM (Node Package Manager)\nNode.js ..."
-  },
-  {
-    "title": "프로그래머스 웹 풀스택 3주 (01월 19일)",
-    "date": "2026. 1. 19.",
-    "url": "https://velog.io/@leekh010502/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%9B%B9-%ED%92%80%EC%8A%A4%ED%83%9D-3%EC%A3%BC-01%EC%9B%94-19%EC%9D%BC",
-    "summary": "Express, REST API 실습, Json\nHTTP vs Express\nHTTP\nNode.js 설치 시 기본적으로 제공되는 가장 기초적인 모듈\nExpress\nHTTP 모듈을 ..."
-  },
-  {
-    "title": "프로그래머스 웹 풀스택 2주 (01월 16일)",
-    "date": "2026. 1. 16.",
-    "url": "https://velog.io/@leekh010502/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%9B%B9-%ED%92%80%EC%8A%A4%ED%83%9D-2%EC%A3%BC-01%EC%9B%94-16%EC%9D%BC",
-    "summary": "Node.js, 모듈, 라이브러리, 프레임워크\nNode.js\n특징\n싱글 스레드\n한 번에 한 가지 일만 처리하는 일꾼이 한 명뿐이지만, 일을 매우 빠르게 번갈아 처리하여 효율적으로 ..."
   }
 ];
