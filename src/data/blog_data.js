47a64aa7a882cc6ab05a1bd7ec17f6ecd82791ac
leekyuhyun <leekh010502@gmail.com>
2026-01-28 04:01:09 +0000

    chore : update latest velog posts

--- src/data/blog_data.js
+++ src/data/blog_data.js
@@ -1,26 +1,26 @@
 export const blogData = [
   {
+    "title": "[Docker 🐳] 도커 볼륨을 활용해 데이터 유실 방지하기",
+    "date": "2026. 1. 27.",
+    "url": "https://velog.io/@leekh010502/Docker-%EB%8F%84%EC%BB%A4-%EB%B3%BC%EB%A5%A8%EC%9D%84-%ED%99%9C%EC%9A%A9%ED%95%B4-%EB%8D%B0%EC%9D%B4%ED%84%B0-%EC%9C%A0%EC%8B%A4-%EB%B0%A9%EC%A7%80%ED%95%98%EA%B8%B0",
+    "summary": "Docker Volume (도커 볼륨)\n컨테이너가 가진 문제점\nDocker를 활용하면 특정 프로그램을 컨테이너로 띄울 수 있다. \n이 프로그램에 기능이 추가되면 새로운 이미지를 만..."
+  },
+  {
+    "title": "로그인, 회원가입 API 설계 및 구현",
+    "date": "2026. 1. 27.",
+    "url": "https://velog.io/@leekh010502/%EB%A1%9C%EA%B7%B8%EC%9D%B8-%ED%9A%8C%EC%9B%90%EA%B0%80%EC%9E%85-API-%EC%84%A4%EA%B3%84-%EB%B0%8F-%EA%B5%AC%ED%98%84",
+    "summary": "회원 API 설계\n그동안 배운 내용을 바탕으로 로그인/회원가입 API를 설계를 하고 postman을 통하여 확인해볼 예정입니다.\n1. 로그인 Post /login\nrequset :..."
+  },
+  {
+    "title": "[week4] 백엔드 기초 : Node.js + Express 기본 (8) - 01/27",
+    "date": "2026. 1. 27.",
+    "url": "https://velog.io/@leekh010502/week4-%EB%B0%B1%EC%97%94%EB%93%9C-%EA%B8%B0%EC%B4%88-Node.js-Express-%EA%B8%B0%EB%B3%B8-8-0127",
+    "summary": "Handler\nHandler란?\n안드로이드에서 필수적인 개념으로 알려져있는 용어\n넓은 의미로 쓰이는 단어라, 쓰는 환경에 따라 뜻하는 의미가 조금씩 다름\n일반적인 큰 의미는, 요청..."
+  },
+  {
     "title": "[TypeScript 🧐] 시작 전..",
     "date": "2026. 1. 26.",
     "url": "https://velog.io/@leekh010502/TypeScript-%EC%8B%9C%EC%9E%91-%EC%A0%84",
     "summary": "친구들과 타입스크립트 스터디를 통하여 타입스크립트 기초를 다지겠습니다..."
-  },
-  {
-    "title": "[React 🔯] 시작 전..",
-    "date": "2026. 1. 26.",
-    "url": "https://velog.io/@leekh010502/React-%EC%8B%9C%EC%9E%91-%EC%A0%84",
-    "summary": "친구들과 리액트 스터디를 하면서 자바스크립트 기초를 다시 잡고 리액트 학습을 열심히 하겠습니다..."
-  },
-  {
-    "title": "[Docker 🐳] 시작 전에..",
-    "date": "2026. 1. 26.",
-    "url": "https://velog.io/@leekh010502/Docker-%EC%8B%9C%EC%9E%91-%EC%A0%84%EC%97%90",
-    "summary": "안녕하세요 7주간 도커 스터디한 내용을 정리하면서 공부 열심히 해보겠습니다..."
-  },
-  {
-    "title": "[Docker 🐳] CLI 익히기",
-    "date": "2026. 1. 26.",
-    "url": "https://velog.io/@leekh010502/Docker-CLI-%EC%9D%B5%ED%9E%88%EA%B8%B0",
-    "summary": "이미지 (Image)\n이미지(Image) 다운로드\n이미지를 다운로드 할 때 Dockerhub라는 곳에서 이미지를 다운 받는다.\n\n최신버전 (latest) 이미지 다운로드\n# doc..."
   }
 ];
