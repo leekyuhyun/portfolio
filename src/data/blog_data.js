595ffeaaff891124fafd0dc8817e7426a1c7166e
leekyuhyun <leekh010502@gmail.com>
2026-01-29 04:22:22 +0000

    chore : update latest velog posts

--- src/data/blog_data.js
+++ src/data/blog_data.js
@@ -1,5 +1,17 @@
 export const blogData = [
   {
+    "title": "채널 API 설계 및 구현",
+    "date": "2026. 1. 28.",
+    "url": "https://velog.io/@leekh010502/%EC%B1%84%EB%84%90-API-%EC%84%A4%EA%B3%84-%EB%B0%8F-%EA%B5%AC%ED%98%84",
+    "summary": "이번에는 채널 API 설계와 구현을 해볼 예정입니다\nAPI 명세\n채널\n채널 생성\n채널 수정\n채널 삭제\n채널 조회\n1. 채널 생성 : Post /channel\nreq : body(c..."
+  },
+  {
+    "title": "[week4] 백엔드 기초 : Node.js + Express 기본 (9) - 01/28",
+    "date": "2026. 1. 28.",
+    "url": "https://velog.io/@leekh010502/week4-%EB%B0%B1%EC%97%94%EB%93%9C-%EA%B8%B0%EC%B4%88-Node.js-Express-%EA%B8%B0%EB%B3%B8-9-0128",
+    "summary": "로그인 API 구현\nAPI 설계\nrequset : body (id,pwd)\nresponse : ${name}님 반갑습니다.\nhttp status\nsuccess (아이디, 비밀번호 ..."
+  },
+  {
     "title": "[Docker 🐳] 도커 볼륨을 활용해 데이터 유실 방지하기",
     "date": "2026. 1. 27.",
     "url": "https://velog.io/@leekh010502/Docker-%EB%8F%84%EC%BB%A4-%EB%B3%BC%EB%A5%A8%EC%9D%84-%ED%99%9C%EC%9A%A9%ED%95%B4-%EB%8D%B0%EC%9D%B4%ED%84%B0-%EC%9C%A0%EC%8B%A4-%EB%B0%A9%EC%A7%80%ED%95%98%EA%B8%B0",
@@ -10,17 +22,5 @@ export const blogData = [
     "date": "2026. 1. 27.",
     "url": "https://velog.io/@leekh010502/%EB%A1%9C%EA%B7%B8%EC%9D%B8-%ED%9A%8C%EC%9B%90%EA%B0%80%EC%9E%85-API-%EC%84%A4%EA%B3%84-%EB%B0%8F-%EA%B5%AC%ED%98%84",
     "summary": "회원 API 설계\n그동안 배운 내용을 바탕으로 로그인/회원가입 API를 설계를 하고 postman을 통하여 확인해볼 예정입니다.\n1. 로그인 Post /login\nrequset :..."
-  },
-  {
-    "title": "[week4] 백엔드 기초 : Node.js + Express 기본 (8) - 01/27",
-    "date": "2026. 1. 27.",
-    "url": "https://velog.io/@leekh010502/week4-%EB%B0%B1%EC%97%94%EB%93%9C-%EA%B8%B0%EC%B4%88-Node.js-Express-%EA%B8%B0%EB%B3%B8-8-0127",
-    "summary": "Handler\nHandler란?\n안드로이드에서 필수적인 개념으로 알려져있는 용어\n넓은 의미로 쓰이는 단어라, 쓰는 환경에 따라 뜻하는 의미가 조금씩 다름\n일반적인 큰 의미는, 요청..."
-  },
-  {
-    "title": "[TypeScript 🧐] 시작 전..",
-    "date": "2026. 1. 26.",
-    "url": "https://velog.io/@leekh010502/TypeScript-%EC%8B%9C%EC%9E%91-%EC%A0%84",
-    "summary": "친구들과 타입스크립트 스터디를 통하여 타입스크립트 기초를 다지겠습니다..."
   }
 ];
