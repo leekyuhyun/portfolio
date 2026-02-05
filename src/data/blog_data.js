df928148b8a9566eabd3176c1099480735f4708b
leekyuhyun <leekh010502@gmail.com>
2026-02-05 04:29:41 +0000

    chore : update latest velog posts

--- src/data/blog_data.js
+++ src/data/blog_data.js
@@ -1,5 +1,11 @@
 export const blogData = [
   {
+    "title": "[week5] 백엔드 심화 : 인증과 비동기 처리(4) - 02/04",
+    "date": "2026. 2. 4.",
+    "url": "https://velog.io/@leekh010502/week5-%EB%B0%B1%EC%97%94%EB%93%9C-%EC%8B%AC%ED%99%94-%EC%9D%B8%EC%A6%9D%EA%B3%BC-%EB%B9%84%EB%8F%99%EA%B8%B0-%EC%B2%98%EB%A6%AC4-0204",
+    "summary": "유효성 검사\n지금까지는 if (user_id && channel_name)와 같이 조건문을 사용해 일일이 입력값을 검증하였지만 하지만 반복되는 코드가 많아지는 불편함이 있었다.\n그..."
+  },
+  {
     "title": "[Docker 🐳] 4. DockerFile을 활용해 이미지 직접 만들기",
     "date": "2026. 2. 3.",
     "url": "https://velog.io/@leekh010502/Docker-4.-DockerFile%EC%9D%84-%ED%99%9C%EC%9A%A9%ED%95%B4-%EC%9D%B4%EB%AF%B8%EC%A7%80-%EC%A7%81%EC%A0%91-%EB%A7%8C%EB%93%A4%EA%B8%B0",
@@ -16,11 +22,5 @@ export const blogData = [
     "date": "2026. 2. 2.",
     "url": "https://velog.io/@leekh010502/week5-%EB%B0%B1%EC%97%94%EB%93%9C-%EC%8B%AC%ED%99%94-%EC%9D%B8%EC%A6%9D%EA%B3%BC-%EB%B9%84%EB%8F%99%EA%B8%B0%EC%B2%98%EB%A6%AC2-0202",
     "summary": "DB 테이블 실습\n저번 시간에 만들었던 게시글과 사용자 테이블을 직접 데이터베이스에 만들도록 하겠습니다.\n\n게시글 테이블에는 사용자 번호가 필요하기 때문에 사용자 테이블부터 만듭니..."
-  },
-  {
-    "title": "[week4] 백엔드 심화 : 인증과 비동기처리(1) - 01/30",
-    "date": "2026. 1. 30.",
-    "url": "https://velog.io/@leekh010502/week4-%EB%B0%B1%EC%97%94%EB%93%9C-%EC%8B%AC%ED%99%94-%EC%9D%B8%EC%A6%9D%EA%B3%BC-%EB%B9%84%EB%8F%99%EA%B8%B0%EC%B2%98%EB%A6%AC1-0130",
-    "summary": "데이터베이스 (Database)\n데이터베이스란?\n데이터를 통합하여 효율적으로 관리하기 위한 데이터 집합체를 데이터베이스 (Database: DB)라고 한다.\n데이터를 구조화하여 관..."
   }
 ];
