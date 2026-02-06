d4fb837d11367a830665399bb2b1c39727f62920
leekyuhyun <leekh010502@gmail.com>
2026-02-06 04:29:57 +0000

    chore : update latest velog posts

--- src/data/blog_data.js
+++ src/data/blog_data.js
@@ -1,5 +1,17 @@
 export const blogData = [
   {
+    "title": "[React 🔯] 02.  JavaScript 심화",
+    "date": "2026. 2. 5.",
+    "url": "https://velog.io/@leekh010502/React-02.-JavaScript-%EC%8B%AC%ED%99%94",
+    "summary": "해당 학습 정리는 이정환 강사님의 한입 크기로 잘라먹는 React기반입니다.\nTruthy & Falsy\n자바스크립트는 어떠한 값이 boolean 타입에 해당하는 참이거나 또는 거짓..."
+  },
+  {
+    "title": "[week5] 백엔드 심화 : 인증과 비동기 처리(5) - 02/05",
+    "date": "2026. 2. 5.",
+    "url": "https://velog.io/@leekh010502/week5-%EB%B0%B1%EC%97%94%EB%93%9C-%EC%8B%AC%ED%99%94-%EC%9D%B8%EC%A6%9D%EA%B3%BC-%EB%B9%84%EB%8F%99%EA%B8%B0-%EC%B2%98%EB%A6%AC5-0205",
+    "summary": "user.js에 validator 추가\n어제 배운 express-validator를 user.js에도 적용해보았습니다.\nconst validate = (req, res, next)..."
+  },
+  {
     "title": "[week5] 백엔드 심화 : 인증과 비동기 처리(4) - 02/04",
     "date": "2026. 2. 4.",
     "url": "https://velog.io/@leekh010502/week5-%EB%B0%B1%EC%97%94%EB%93%9C-%EC%8B%AC%ED%99%94-%EC%9D%B8%EC%A6%9D%EA%B3%BC-%EB%B9%84%EB%8F%99%EA%B8%B0-%EC%B2%98%EB%A6%AC4-0204",
@@ -10,17 +22,5 @@ export const blogData = [
     "date": "2026. 2. 3.",
     "url": "https://velog.io/@leekh010502/Docker-4.-DockerFile%EC%9D%84-%ED%99%9C%EC%9A%A9%ED%95%B4-%EC%9D%B4%EB%AF%B8%EC%A7%80-%EC%A7%81%EC%A0%91-%EB%A7%8C%EB%93%A4%EA%B8%B0",
     "summary": "DockerFile이란?\nDocker 이미지를 만들게 해주는 파일이다.\nFROM\nFROM은 베이스 이미지를 생성하는 역할을 한다.\nDocker 컨테이너를 특정 초기 이미지 기반으로..."
-  },
-  {
-    "title": "[week5] 백엔드 심화 : 인증과 비동기처리(3) - 02/03",
-    "date": "2026. 2. 3.",
-    "url": "https://velog.io/@leekh010502/week5-%EB%B0%B1%EC%97%94%EB%93%9C-%EC%8B%AC%ED%99%94-%EC%9D%B8%EC%A6%9D%EA%B3%BC-%EB%B9%84%EB%8F%99%EA%B8%B0%EC%B2%98%EB%A6%AC3-0203",
-    "summary": "DB 연동 실습\n이번 시간에는 유튜브 (채널 & 회원) API 설계했던것을 DB와 연동하는 실습을 해볼 예정입니다.\n1. DB에 테이블 생성\n회원(Users) 테이블\n\n채널(Cha..."
-  },
-  {
-    "title": "[week5] 백엔드 심화 : 인증과 비동기처리(2) - 02/02",
-    "date": "2026. 2. 2.",
-    "url": "https://velog.io/@leekh010502/week5-%EB%B0%B1%EC%97%94%EB%93%9C-%EC%8B%AC%ED%99%94-%EC%9D%B8%EC%A6%9D%EA%B3%BC-%EB%B9%84%EB%8F%99%EA%B8%B0%EC%B2%98%EB%A6%AC2-0202",
-    "summary": "DB 테이블 실습\n저번 시간에 만들었던 게시글과 사용자 테이블을 직접 데이터베이스에 만들도록 하겠습니다.\n\n게시글 테이블에는 사용자 번호가 필요하기 때문에 사용자 테이블부터 만듭니..."
   }
 ];
