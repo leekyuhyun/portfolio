b9580462ef0574df4830fb6a065e0174d2324149
leekyuhyun <leekh010502@gmail.com>
2026-02-06 09:02:46 +0000

    chore : update latest velog posts

--- src/data/blog_data.js
+++ src/data/blog_data.js
@@ -1,26 +1,26 @@
 export const blogData = [
   {
+    "title": "[React 🔯] 04. React.js 개론",
+    "date": "2026. 2. 6.",
+    "url": "https://velog.io/@leekh010502/React-04.-React.js-%EA%B0%9C%EB%A1%A0",
+    "summary": "해당 학습 정리는 이정환 강사님의 한입 크기로 잘라먹는 React기반으로 작성하였습니다.\nReact.js를 소개합니다.\nReact.js란?\nMeta(Facebook)이 개발한 오픈..."
+  },
+  {
+    "title": "[React 🔯] 03. Node.js 기초",
+    "date": "2026. 2. 6.",
+    "url": "https://velog.io/@leekh010502/React-03.-Node.js-%EA%B8%B0%EC%B4%88",
+    "summary": "해당 학습 정리는 이정환 강사님의 한입 크기로 잘라먹는 React기반으로 작성하였습니다.\nReact.js 배우는데  갑자기 Node.js??\nReact.js라는 기술은 Node.j..."
+  },
+  {
     "title": "[React 🔯] 02.  JavaScript 심화",
     "date": "2026. 2. 5.",
     "url": "https://velog.io/@leekh010502/React-02.-JavaScript-%EC%8B%AC%ED%99%94",
-    "summary": "해당 학습 정리는 이정환 강사님의 한입 크기로 잘라먹는 React기반입니다.\nTruthy & Falsy\n자바스크립트는 어떠한 값이 boolean 타입에 해당하는 참이거나 또는 거짓..."
+    "summary": "해당 학습 정리는 이정환 강사님의 한입 크기로 잘라먹는 React기반으로 작성하였습니다.\nTruthy & Falsy\n자바스크립트는 어떠한 값이 boolean 타입에 해당하는 참이거..."
   },
   {
     "title": "[week5] 백엔드 심화 : 인증과 비동기 처리(5) - 02/05",
     "date": "2026. 2. 5.",
     "url": "https://velog.io/@leekh010502/week5-%EB%B0%B1%EC%97%94%EB%93%9C-%EC%8B%AC%ED%99%94-%EC%9D%B8%EC%A6%9D%EA%B3%BC-%EB%B9%84%EB%8F%99%EA%B8%B0-%EC%B2%98%EB%A6%AC5-0205",
     "summary": "user.js에 validator 추가\n어제 배운 express-validator를 user.js에도 적용해보았습니다.\nconst validate = (req, res, next)..."
-  },
-  {
-    "title": "[week5] 백엔드 심화 : 인증과 비동기 처리(4) - 02/04",
-    "date": "2026. 2. 4.",
-    "url": "https://velog.io/@leekh010502/week5-%EB%B0%B1%EC%97%94%EB%93%9C-%EC%8B%AC%ED%99%94-%EC%9D%B8%EC%A6%9D%EA%B3%BC-%EB%B9%84%EB%8F%99%EA%B8%B0-%EC%B2%98%EB%A6%AC4-0204",
-    "summary": "유효성 검사\n지금까지는 if (user_id && channel_name)와 같이 조건문을 사용해 일일이 입력값을 검증하였지만 하지만 반복되는 코드가 많아지는 불편함이 있었다.\n그..."
-  },
-  {
-    "title": "[Docker 🐳] 4. DockerFile을 활용해 이미지 직접 만들기",
-    "date": "2026. 2. 3.",
-    "url": "https://velog.io/@leekh010502/Docker-4.-DockerFile%EC%9D%84-%ED%99%9C%EC%9A%A9%ED%95%B4-%EC%9D%B4%EB%AF%B8%EC%A7%80-%EC%A7%81%EC%A0%91-%EB%A7%8C%EB%93%A4%EA%B8%B0",
-    "summary": "DockerFile이란?\nDocker 이미지를 만들게 해주는 파일이다.\nFROM\nFROM은 베이스 이미지를 생성하는 역할을 한다.\nDocker 컨테이너를 특정 초기 이미지 기반으로..."
   }
 ];
